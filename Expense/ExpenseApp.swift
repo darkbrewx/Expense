c4b38c352c0171ede5a42369a3c0264d37f0b048
WENBO HU <darkbrewx@gmail.com>
2023-07-03 11:27:09 +0900

    Initial Commit

---
+++ Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by 胡文博 on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
